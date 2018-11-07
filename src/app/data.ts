b5275a65f685a55b7c75fca33b6cebbf1b32303c
davyengone <davy@hackages.io>
2018-11-07 15:06:28 +0100

    added data

---
+++ src/app/data.ts
@@ -0,0 +1,15 @@
+export const cdk = [
+  'Accessibility',
+  'Bidirectionality',
+  'Table',
+  'Drag and Drop',
+  'Layout',
+  'Observers',
+  'Overlay',
+  'Tree',
+  'Platform',
+  'Portal',
+  'Scrolling',
+  'Stepper',
+  'Text field'
+];
