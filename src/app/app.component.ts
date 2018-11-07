07a826b96ee1f4cbe03a7ca75308bb9f68186932
davyengone <davy@hackages.io>
2018-11-07 17:25:08 +0100

    starting point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
+import { cdk } from './data';
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  cdkItems: string[] = cdk;
+  components: string[] = [];
+  behaviors: string[] = [];
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(event);
+    moveItemInArray(this.cdkItems, event.previousIndex, event.currentIndex);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, DragDropModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
